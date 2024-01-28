64bd00227c9d9f3887f85401c18e5ea248fdd10a
pscorp28 <palomares.fernanda@gmail.com>
2024-01-28 13:58:04 -0600

    ribodepletionfiles&kneaddata

--- ribodepletion-only-assembled.sh
+++ ribodepletion-only-assembled.sh
@@ -7,14 +7,14 @@ rm -r $HOME/sortmerna
 META=$HOME/metatranscriptomics
 DATABASES="$META/databases"
 SORTMERNA_DB="$DATABASES/rRNA_database"
-MERGED_FILES="$META/02_assembled-reads"
-RIBODEPLETED_FILES="$META/03_ribodepleted-reads"
-RIBOSOMAL_FILES="$META/03.1_rrna-reads"
+MERGED_FILES="$META/02_only_merged_reads"
+RIBODEPLETED_FILES="$META/03_ribodepleted_only_merged"
+RIBOSOMAL_FILES="$META/03.2_rrna-reads"
 
-for file in $MERGED_FILES/*.assembled2.fastq
+for file in $MERGED_FILES/*.assembled.fastq
 do
 rm -r $HOME/sortmerna
-shortname=`echo $file | awk -F "assembled2" '{print $1 "ribodepleted"}'`
+shortname=`echo $file | awk -F "assembled" '{print $1 "ribodepleted"}'`
 sortmerna -ref $SORTMERNA_DB/rfam-5.8s-database-id98.fasta \
 -ref $SORTMERNA_DB/rfam-5s-database-id98.fasta \
 -ref $SORTMERNA_DB/silva-arc-16s-id95.fasta \
@@ -27,4 +27,3 @@ sortmerna -ref $SORTMERNA_DB/rfam-5.8s-database-id98.fasta \
 done
 
 mv $MERGED_FILES/*ribodeplete* $RIBODEPLETED_FILES
-
